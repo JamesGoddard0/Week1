3638a1a0a203c05818d7ee2df764510e66bdb0ec
James Goddard <JamesMGoddard29@gmail.com>
2021-01-05 11:54:49 -0800

    Initial commit

---
+++ animal.py
@@ -0,0 +1,6 @@
+# Author: James Goddard
+# Date: 1/5/2021
+# Description: A program that prints out the users favorite animal after input.
+
+fav_animal = input("Enter the name of your fav animal: ")
+print("Your favorite animal is: " + fav_animal + ".")
